e3801ec01761f52dc7ffee971e673c5735da891b
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-02-12 13:20:15 -0500

    text

---
+++ things/firstScript.js
@@ -0,0 +1,43 @@
+console.log("nothing broken yet.")
+    //ALERT("nothing broken yet.") Good but annoying. Console isn't as annoying
+
+    //$(".menu-area").addClass("active")
+
+    $(".menu-area").click(function(){
+
+    
+          //  if ( $(".menu-area").hasClass("active")  ) {
+          //     $(".menu-area").removeClass("active")
+          //  } else {
+          //      $(".menu-area").addClass("active")
+          //  }
+
+          
+          //THE EASY WAY
+
+          $(".menu-area").toggleClass("active")
+    
+    })
+
+
+
+
+
+
+
+
+//var studentName = "Dina";
+//studentName = studentName+" Tsyuy"
+//alert("Hello, "+ studentName);
+
+
+
+
+
+
+
+    /*
+      function(){ //NOTHING GOES BETWEEN PARANTHESES AT THE MOMENT
+    WHATEVER YOU WANT TO HAPPEN IN HERE IS YOUR FUNCTION
+        }
+        */
---
+++ things/textScript.js
@@ -0,0 +1,8 @@
+console.log("nothing broken yet.")
+    //ALERT("nothing broken yet.") Good but annoying. Console isn't as annoying
+
+	$(".title-area").click(function(){
+
+		$(".title-area").toggleClass("active")
+    
+    })
