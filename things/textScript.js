9d722056175ef2c74ea34fc2d122e4acd4788249
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-02-23 11:02:45 -0500

    text

--- things/textScript.js
+++ things/textScript.js
@@ -7,4 +7,35 @@ console.log("nothing broken yet.")
     
     })
 
-   
+//$(document).scroll(function(){
+
+	//docHeight = $(document).height();
+	//winHeight = $(window).height();
+   //	fromTop = $(document).scrollTop();
+
+   //	scrollRatio = fromTop/(docHeight-winHeight)
+
+   //$(".pre-people")
+   			//.css({
+   				//"opacity":scrollRatio*2,
+   			
+   				//"transform":"movingleft("+(500*scrollRatio)+"px)"
+ 
+
+   		//});
+
+   		
+   		//if(scrollRatio>0.5){
+   			//$(".pre-people").attr("src","post_people_nyc.jpg")
+   			//.css({
+   				//"left":25+500*(scrollRatio-0.5)
+
+   			//})
+   		//}	
+   	
+
+   	//console.log(scrollRatio);
+   		
+   		
+
+//})
